cf3f37c6caa5018fa5df2ec0f6c7b751c1492c70
mbuccat <mbuccat@bu.edu>
2020-06-02 10:54:59 -0400

    Lint index.js

--- server/index.js
+++ server/index.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const assert = require('assert');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const uri = process.env.DATABASE_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
-client.connect(err => {
-    assert.equal(null, err);
-    console.log("Connected successfully to the database");
+client.connect((err) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to the database');
 
-    const db = client.db("acchord");
+  const db = client.db('acchord');
 
-    client.close();
+  client.close();
 });
 
 app.use(morgan('dev'));
 app.use(helmet());
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+  res.send('Hello World!');
 });
 
 app.get('/*', (req, res) => {
-    res.status(404);
-    res.send('Page not found');
-})
+  res.status(404);
+  res.send('Page not found');
+});
 
 app.listen(port, () => {
-    console.log(`Listening at ${port}`);
+  console.log(`Listening at ${port}`);
 });
