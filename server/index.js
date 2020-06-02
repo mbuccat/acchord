6f4ee8a311c83e0024e2a39b6b532ee35ff3e57f
mbuccat <mbuccat@bu.edu>
2020-06-02 09:35:42 -0400

    Connect the mongo database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(morgan('dev'));
 app.use(helmet());
