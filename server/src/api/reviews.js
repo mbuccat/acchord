c74ec9945dc5cf9356cfb7c1cf4c417d2c055b89
mbuccat <mbuccat@bu.edu>
2020-06-06 15:00:53 -0400

    Connect reviews api to database

--- server/src/api/reviews.js
+++ server/src/api/reviews.js
@@ -1,24 +1,57 @@
 const { Router } = require('express');
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
 const router = Router();
 
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+client.connect((err) => {
+  assert.equal(null, err);
+  console.log('Successfully connected to the database');
+});
+
+
 router.get('/', (req, res) => {
   try {
-    res.json({
-      message: 'Test',
+    const db = client.db('acchord');
+    const collection = db.collection('reviews');
+    const cursor = collection.find();
+    const results = [];
+
+    cursor.forEach((doc, docErr) => {
+      assert.equal(null, docErr);
+      results.push(doc);
+    }, () => {
+      res.status(200);
+      res.json({ results });
     });
   } catch (err) {
+    res.sendStatus(500);
     console.log(err.message);
   }
 });
 
 router.post('/', (req, res) => {
   try {
-    console.log(req.body);
-    res.json({
-      message: 'Post received',
+    const db = client.db('acchord');
+    const collection = db.collection('reviews');
+    const { mediaType, mediaName, mediaCreator, content, created } = req.body;
+    const reviewData = {
+      mediaType,
+      mediaName,
+      mediaCreator,
+      content,
+      created,
+    };
+
+    collection.insertOne(reviewData, (insertionErr) => {
+      assert.equal(null, insertionErr);
+      console.log('Item inserted into database');
+      res.sendStatus(200);
     });
   } catch (err) {
+    res.sendStatus(500);
     console.log(err.message);
   }
 });
--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
-const assert = require('assert');
 
 require('dotenv').config();
 
@@ -12,17 +10,6 @@ const reviews = require('./api/reviews');
 
 const app = express();
 const port = process.env.PORT || 3001;
-
-const uri = process.env.DATABASE_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
-client.connect((err) => {
-  assert.equal(null, err);
-  console.log('Connected successfully to the database');
-
-  const db = client.db('acchord');
-
-  client.close();
-});
 
 app.use(morgan('common'));
 app.use(helmet());
