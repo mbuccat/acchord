67921be4f7cdd60c58d57ac311347187c67fdac5
mbuccat <mbuccat@bu.edu>
2020-06-15 17:56:34 -0400

    Updates

---
+++ precache-manifest.042fc365de3e8bf5ad603f273c534222.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f81750578f8285f11d7d2c835d93819",
+    "url": "/acchord/index.html"
+  },
+  {
+    "revision": "117ac854eb547b7ea04d",
+    "url": "/acchord/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "67e2a726cc34649815a9",
+    "url": "/acchord/static/css/main.815a9b6e.chunk.css"
+  },
+  {
+    "revision": "117ac854eb547b7ea04d",
+    "url": "/acchord/static/js/2.57a0a588.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acchord/static/js/2.57a0a588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e2a726cc34649815a9",
+    "url": "/acchord/static/js/main.bcc2982a.chunk.js"
+  },
+  {
+    "revision": "7318b1e976fdb0745ae7",
+    "url": "/acchord/static/js/runtime-main.865e4dae.js"
+  },
+  {
+    "revision": "27544c82b2824b7454874b17bb4c1de0",
+    "url": "/acchord/static/media/album.27544c82.png"
+  },
+  {
+    "revision": "91c3ee310bd365d42b0968ae3e4ec305",
+    "url": "/acchord/static/media/track.91c3ee31.png"
+  }
+]);
